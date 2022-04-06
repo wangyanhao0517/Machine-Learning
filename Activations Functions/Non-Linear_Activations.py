2310c56cabfd808fa29f00c27b86c286a19b8363
wyh <wangyanhao0517@163.com>
2022-04-06 17:20:05 +0800

    add Activation Functions dir

---
+++ Activations Functions/Non-Linear_Activations.py
@@ -0,0 +1,8 @@
+'''
+Descripttion: 
+version: 
+Author: wyh
+Date: 2022-04-06 17:17:56
+LastEditors: wyh
+LastEditTime: 2022-04-06 17:17:58
+'''
---
+++ Activations Functions/Step_Functions.py
@@ -0,0 +1,8 @@
+'''
+Descripttion: 
+version: 
+Author: wyh
+Date: 2022-04-06 17:18:42
+LastEditors: wyh
+LastEditTime: 2022-04-06 17:18:42
+'''
